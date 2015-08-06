3e26cea2060a3b73f784ea4562a974edbe628a93
Damian Papiez <damian.papiez@nokia.com>
2015-08-06 13:18:55 +0200

    LMTS-init telnet connection/config update

--- LMTS_init.py
+++ LMTS_init.py
@@ -2,16 +2,14 @@
 """
 :created on: '6/8/15'
 
-:copyright: NSN
 :author: Damian Papiez
 """
-
-__author__ = 'ute'
 
 import pexpect
 import sys
 from time import sleep
 
+#LMTS-executor start
 child_bash = pexpect.spawn("/bin/bash")
 child_bash.sendline("sudo /etc/init.d/lmts-executor start")
 try:
@@ -33,8 +31,9 @@ if i == 1:
     if i == 1:
         print "LMTS-executor start fail"
         sys.exit(0);
+sleep (2)
 
-
+#LMTS-trace start
 child_bash.sendline("sudo /etc/init.d/lmts-trace start")
 try:
     child_bash.expect("Starting up LMTS trace")
@@ -45,3 +44,22 @@ except pexpect.TIMEOUT:
 if i == 1:
     print "LMTS-trace start fail"
     sys.exit(0);
+sleep (2)
+
+#start telnet connection
+child_bash.sendline("telnet 0 1234")
+try:
+    child_bash.expect("Connected")
+    child_bash.expect("LTE-LMTS>")
+except pexpect.TIMEOUT:
+    print "pexpect.TIMEOUT - telnet connection"
+    sys.exit(0)
+
+#config update
+child_bash.sendline("config update")
+try:
+    child_bash.expect("New configuration applied")
+    child_bash.expect("LTE-LMTS>")
+except pexpect.TIMEOUT:
+    print "pexpect.TIMEOUT - config update"
+    sys.exit(0)
