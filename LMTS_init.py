dfd96475b3bb450f3f7d08951528c4918318b970
Damian Papiez <damian.papiez@nokia.com>
2015-08-06 11:21:14 +0200

    LMTS-init lmts-executor start

--- LMTS_init.py
+++ LMTS_init.py
@@ -6,24 +6,31 @@
 :author: Damian Papiez
 """
 
+__author__ = 'ute'
+
 import pexpect
 import sys
 from time import sleep
 
 child_bash = pexpect.spawn("/bin/bash")
-'''
-child_bash.sendline("sudo /etc/init.d/lmts-executor stop")
-child_bash.expect("\n")
-child_bash.expect("\n")
-child_bash.expect("\n")
-print child_bash.before
-'''
 child_bash.sendline("sudo /etc/init.d/lmts-executor start")
 child_bash.expect("Starting up LMTS Executor:")
 i = child_bash.expect(["\.", "failed", pexpect.TIMEOUT])
 
 if i == 1:
-    print "failed"
+    child_bash.sendline("sudo /etc/init.d/lmts-executor stop")
+    sleep(1)
+    child_bash.sendline("sudo /etc/init.d/lmts-executor start")
+    child_bash.expect("Starting up LMTS Executor:")
+    i = child_bash.expect(["\.", "failed", pexpect.TIMEOUT])
+    if i == 1:
+        print "LMTS-executor start fail"
+        sys.exit(0);
+    elif i == 2:
+        print "Time out!"
+        sys.exit(0);
 elif i == 2:
     print "Time out!"
+    sys.exit(0);
+
 
