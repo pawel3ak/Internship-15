710804db7888eaf6c8d932c37d76c109e94ae59d
Damian Papiez <damian.papiez@nokia.com>
2015-08-06 11:44:16 +0200

    LMTS-init lmts-trace start

--- LMTS_init.py
+++ LMTS_init.py
@@ -14,23 +14,34 @@ from time import sleep
 
 child_bash = pexpect.spawn("/bin/bash")
 child_bash.sendline("sudo /etc/init.d/lmts-executor start")
-child_bash.expect("Starting up LMTS Executor:")
-i = child_bash.expect(["\.", "failed", pexpect.TIMEOUT])
-
+try:
+    child_bash.expect("Starting up LMTS Executor:")
+    i = child_bash.expect(["\.", "failed", pexpect.TIMEOUT])
+except pexpect.TIMEOUT:
+    print "pexpect.TIMEOUT - starting up LMTS Executor"
+    sys.exit(0)
 if i == 1:
     child_bash.sendline("sudo /etc/init.d/lmts-executor stop")
     sleep(1)
     child_bash.sendline("sudo /etc/init.d/lmts-executor start")
-    child_bash.expect("Starting up LMTS Executor:")
-    i = child_bash.expect(["\.", "failed", pexpect.TIMEOUT])
+    try:
+        child_bash.expect("Starting up LMTS Executor:")
+        i = child_bash.expect(["\.", "failed"])
+    except pexpect.TIMEOUT:
+        print "pexpect.TIMEOUT - starting up LMTS Executor"
+        sys.exit(0)
     if i == 1:
         print "LMTS-executor start fail"
         sys.exit(0);
-    elif i == 2:
-        print "Time out!"
-        sys.exit(0);
-elif i == 2:
-    print "Time out!"
+
+
+child_bash.sendline("sudo /etc/init.d/lmts-trace start")
+try:
+    child_bash.expect("Starting up LMTS trace")
+    i = child_bash.expect(["\.", "failed", "process already running"])
+except pexpect.TIMEOUT:
+    print "pexpect.TIMEOUT - starting up LMTS Executor"
+    sys.exit(0)
+if i == 1:
+    print "LMTS-trace start fail"
     sys.exit(0);
-
-
