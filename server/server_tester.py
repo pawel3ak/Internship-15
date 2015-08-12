457c3a36725fa248183ecc828ac171423f898691
Pawel Nogiec <pawel.nogiec@nokia.com>
2015-08-12 14:54:07 +0200

    server files from internship-14

---
+++ server/server_tester.py
@@ -0,0 +1,23 @@
+import socket
+from os import fork
+
+
+host = "0.0.0.0"
+port = 50000
+host = (host,port)
+data = "zapytani"
+
+sock = socket.socket(family= socket.AF_INET, type= socket.SOCK_DGRAM)
+sock.bind(host)
+a = fork()
+if a == 0:
+    while True:
+        sock.connect
+        sock.sendto(data,host)
+        from time import sleep
+        sleep(1)
+else:
+    while True:
+        data,addr = sock.recvfrom(1024)
+        print "data = {data} \naddr={addr}".format(data=data, addr=addr)
+
