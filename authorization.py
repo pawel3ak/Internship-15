1b0421cd6030127766a1a6cc50a35638dedfa583
Pawel Nogiec <pawel.nogiec@nokia.com>
2015-08-12 12:23:06 +0200

    removed old files

--- authorization.py
+++
@@ -1,8 +0,0 @@
-import urllib2
-import base64
-
-def authorization(url, user = None, password = None): #TODO how user/pswd You've used?
-    req = urllib2.Request(url)
-    base64string = base64.encodestring('%s:%s' % (user, password)).replace('\n', '')
-    req.add_header("Authorization", "Basic %s" % base64string)
-    return req
