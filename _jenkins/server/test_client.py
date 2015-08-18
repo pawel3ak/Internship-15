33caccdd2ee825b845da2667b3827c26201e83bd
Pawel Nogiec <pawel.nogiec@nokia.com>
2015-08-18 08:02:33 +0200

    test_client script for server

--- _jenkins/server/test_client.py
+++ _jenkins/server/test_client.py
@@ -9,25 +9,37 @@
 
 import socket
 import sys
+import json
 
 HOST_IP, HOST_PORT = "127.0.0.1", 5005
+request = "request/create_reservation"
+data = json.dumps({'reservation_data' :
+                       {'testline_type' : 'CLOUD_F'},
+                   'user_info' :
+                       {'first_name'    : 'Pawel',
+                        'last_name'     : 'Nogiec',
+                        'e-mail'        : 'pawel.nogiec@nokia.com'},
+                   'jenkins_info' :
+                       {'name' : 'test_job_tl_99',
+                        'parameters' :
+                            {'f_name'   : 'pierwszy',
+                             't1'       : 'drugi',
+                             't2'       : 'trzeci'}
+                       },
+                   })
 
-if sys.argv > 1:
-        data = ""
-        for message in sys.argv[1:]:
-            pass
-            data += str(message) + " "
 
 sock = socket.socket(socket.AF_INET,
                      socket.SOCK_STREAM)
 sock.connect((HOST_IP, HOST_PORT))
-sock.send(data)
+sock.send(request)
 while True:
         response = sock.recv(1024)
-        if response == "":
+        if response == 'OK':
+            sock.send(data)
+        elif response == "":
             break
         print response,
-
 sock.close()
 
 
