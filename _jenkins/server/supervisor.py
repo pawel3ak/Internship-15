89a976c4dabe9aaf950e8bc95713ba5bbccdde47
Pawel Nogiec <pawel.nogiec@nokia.com>
2015-08-20 08:47:53 +0200

    parser in supervisor updated

--- _jenkins/server/supervisor.py
+++ _jenkins/server/supervisor.py
@@ -48,20 +48,28 @@ def _get_tl_name(id):
     reservation = tlr.TestLineReservation(id)
     return reservation.get_reservation_details()['testline']['name']
 
-def send_information(id,tl_name,user_info, test_passed = False):
+def send_information(id,tl_name,user_info, test_passed = None):
     # user_info['first_name']
     # user_info['last_name']
     # user_info['e-mail']
-    _message = "Hello {f_name} {l_name}! \n\n" \
-                  "Your reservation is pending.\n" \
-                  "Reservation ID = {rID}\n" \
-                  "Testline name = {tl_name}\n\n" \
-                  "Have a nice day!".format(f_name=user_info['first_name'],
-                                            l_name=user_info['last_name'],
-                                            rID=id, tl_name=tl_name)
-    _subject = "Reservation status update"
+    if test_passed == None:
+        _message = "Hello {f_name} {l_name}! \n\n" \
+                   "Your reservation is pending.\n" \
+                   "Reservation ID = {rID}\n" \
+                   "Testline name = {tl_name}\n\n" \
+                   "Have a nice day!".format(f_name=user_info['first_name'],
+                                             _name=user_info['last_name'],
+                                             rID=id, tl_name=tl_name)
+        _subject = "Reservation status update"
 
-    if test_passed == True:
+    elif test_passed == False:
+        _message = "Hello {f_name} {l_name}! \n\n" \
+                  "All your tests were successful\n\n" \
+                  "Have a nice day!".format(f_name=user_info['first_name'],
+                                            l_name=user_info['last_name'])
+        _subject = "Tests status update"
+
+    elif test_passed == True:
         _message = "Hello {f_name} {l_name}! \n\n" \
                   "All your tests were successful\n\n" \
                   "Have a nice day!".format(f_name=user_info['first_name'],
@@ -92,28 +100,38 @@ def _create_and_build_job(jenkins_info, tl_name):
 def _get_jenkins_console_output(jenkins_info, job):
     time.sleep(5)       #let the new job build get started
     while job.get_last_build().get_status() == None:
-        time.sleep(10)      ###############TODO longer sleep on real tests
-    return (job.get_last_build().get_console()).split("\n")
+        time.sleep(30)      ###############TODO longer sleep on real tests
+    return job.get_last_build().get_console()
 
-def _update_parent_dict(serverID, parent_dict, id, job_test_status=None, busy_status = False):
-    parent_dict[serverID] = {'reservationID' : id,
-                             'busy' : busy_status,
-                             'test_status' : job_test_status}
+def _update_parent_dict(serverID, parent_dict, id, busy_status, tl_name, duration, job_test_status=None):
+    parent_dict[serverID]['reservationID'] = id
+    parent_dict[serverID]['busy'] = busy_status
+    parent_dict[serverID]['tl_name'] = tl_name
+    parent_dict[serverID]['duration'] = duration
+    parent_dict[serverID]['test_status'] = job_test_status
+
 
 def _get_job_test_status(job_output):
-    dict = {}
     has_got_fail = False
-    for test in range(0,len(job_output)):
+    regex = r'[=]\s(.*)\s[=]\W*.*FAIL'
+    match = re.findall(regex,job_output)
+    if len(match) != 0: has_got_fail = True
+    tests_dict=[]
+    for i in range(0,len(match)):
+        match[i] = re.sub(" +", "_", match[i])
+        if match[i][-3:] == '...': match[i] = match[i][:-3]
         try:
-            match = re.search('^(.*)\s*.\sFAIL|^(.*)\s*.\sPASS',job_output[test]).groups()
-            if match[0] != None:
-                dict[match[0]] = 'FAIL'
-                has_got_fail = True
-            else:
-                dict[str(match[1][:-1]).strip(" ")] = 'PASS'
+            if re.search('^(T.*).*',(match[i].split('.',2))[2]).groups() != None:
+                match[i] = match[i].split('.',3)
+                tests_dict.append({'test_name' : match[i][1],
+                              'file_name' : match[i][3]})
         except:
-            pass
-    return dict, has_got_fail
+            match[i] = match[i].split('.',2)
+            tests_dict.append({'test_name' : match[i][1],
+                              'file_name' : match[i][2]})
+
+
+    return tests_dict, has_got_fail
 
 
 def main(serverID, reservation_data, parent_dict, user_info, jenkins_info):
@@ -125,24 +143,35 @@ def main(serverID, reservation_data, parent_dict, user_info, jenkins_info):
                                                    reservation_data['robotle_revision'],
                                                    reservation_data['state'],
                                                    reservation_data['duration'])
-    print reservationID
-    _update_parent_dict(serverID=serverID, parent_dict=parent_dict, id=reservationID, busy_status=True)
+    _update_parent_dict(serverID=serverID, parent_dict=parent_dict, id=reservationID, busy_status=True,
+                        tl_name='', duration=reservation_data['duration'])
     if not reservation_status(reservationID) == 0:
         raise "Some weird exception"
     tl_name = _get_tl_name(reservationID)
-    print tl_name
-    send_information(reservationID, tl_name, user_info)
+    _update_parent_dict(serverID=serverID, parent_dict=parent_dict, id=reservationID, busy_status=True,
+                        tl_name=tl_name, duration=reservation_data['duration'])
+    #send_information(reservationID, tl_name, user_info)
     job = _create_and_build_job(jenkins_info, tl_name)
     jenkins_console_output = _get_jenkins_console_output(jenkins_info, job)
     job_test_status_dict, has_got_fail = _get_job_test_status(job_output=jenkins_console_output)
     if has_got_fail:
         _update_parent_dict(serverID=serverID, parent_dict=parent_dict, id=reservationID,
-                            job_test_status=job_test_status_dict, busy_status=True)
-        #todo rerun robot with specifig tags
+                            busy_status=False, tl_name=tl_name, duration=reservation_data['duration'],
+                            job_test_status=job_test_status_dict)
+        send_information(id=reservationID, tl_name=tl_name,
+                         user_info={'first_name' : 'Pawel',
+                                    'last_name' : 'Nogiec',
+                                    'e-mail' : 'pawel.nogiec@nokia.com'},
+                         test_passed=False)
     else:
-        _update_parent_dict(serverID=serverID, parent_dict=parent_dict, id=reservationID,
-                            job_test_status=job_test_status_dict, busy_status=False)
-        send_information(reservation_data, tl_name, user_info, test_passed=True)
+        _update_parent_dict(serverID=serverID, parent_dict=parent_dict, id=reservationID, busy_status=True,
+                        tl_name=tl_name, duration=reservation_data['duration'],
+                        job_test_status='ALL tests passed')
+        send_information(id=reservationID, tl_name=tl_name,
+                         user_info={'first_name' : 'Pawel',
+                                    'last_name' : 'Nogiec',
+                                    'e-mail' : 'pawel.nogiec@nokia.com'},
+                         test_passed=True)
 
 
 # if __name__ == '__main__':
