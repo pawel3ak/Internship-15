a5da7492ffd54cea78f1da785bd6f748fbf5c43d
Pawel Nogiec <pawel.nogiec@nokia.com>
2015-08-21 13:13:04 +0200

    new request to the server added

--- _jenkins/server/requests_client.py
+++ _jenkins/server/requests_client.py
@@ -26,15 +26,26 @@ def get_all(sock):
     sock.close()
 
 def get_info(sock, id):
+    sock.send("request/get_info")
     while True:
         response = sock.recv(1024)
-        print response
-        if response == 'OK':
+        try:
+            id = int(id)
+            if not response == "OK": break
             sock.send(str(id))
             response = sock.recv(1024)
+            if response == "": break
             print response
-        elif response == "":
+        except:
             break
+
+
+        # if response == 'OK':
+        #     sock.send(str(id))
+        #     response = sock.recv(1024)
+        #     print response
+        # elif response == "":
+        #     break
     sock.close()
 
 def main():
@@ -47,7 +58,7 @@ def main():
     sock.connect((HOST_IP, HOST_PORT))
     if len(sys.argv) == 1:
         get_all(sock)
-    elif len(sys.argv) == 2:
+    elif len(sys.argv) == 3:
         if sys.argv[1] == "info":
             get_info(sock,sys.argv[2])
 
