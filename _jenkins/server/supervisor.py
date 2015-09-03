988bbea3d38966c257f7c8bd91208cc454a77ab1
Pawel Nogiec <pawel.nogiec@nokia.com>
2015-09-03 18:59:46 +0200

    New approach to the old thing :)

--- _jenkins/server/supervisor.py
+++ _jenkins/server/supervisor.py
@@ -15,15 +15,15 @@ def supervise(serverID, reservation_data, parent_dict, jenkins_info, user_info =
     if supervisor_api.TLreservationID == None:
         supervisor_api.TLreservationID = supervisor_api.create_reservation(
             testline_type=reservation_data['testline_type'],
-            # reservation_data['enb_build'],        #no idea if we'll use this, so I'm leaving that for now
+            # reservation_data['enb_build'],        #no idea if we'll use this, so I'm leaving that temporarily
             # reservation_data['ute_build'],
             # reservation_data['sysimage_build'],
             # reservation_data['robotle_revision'],
             # reservation_data['state'],
             duration=reservation_data['duration'])
-
+    print supervisor_api.TLreservationID
     supervisor_api.set_parent_dictionary(busy_status=True)
-    supervisor_api.check_and_wait_for_TL_being_prepared_to_use()
+    # supervisor_api.check_and_wait_for_TL_being_prepared_to_use()
     supervisor_api.set_TLname_from_TLreservationID()
     supervisor_api.set_TLaddress(supervisor_api.get_TLaddress_from_ID())
 
@@ -36,14 +36,12 @@ def supervise(serverID, reservation_data, parent_dict, jenkins_info, user_info =
 
     supervisor_api.set_parent_dictionary()
 
-    print supervisor_api.git_launch(pull_only=True)
-
-
+    # print supervisor_api.git_launch(pull_only=True)
 
     supervisor_api.set_job_api()
-    if not supervisor_api.is_queued_or_running():
+    if not supervisor_api._is_queued_or_running():
         supervisor_api.create_and_build_job()
-
+    # print "cos"
     supervisor_api.get_jenkins_console_output()
     supervisor_api.get_job_status()
 
@@ -52,11 +50,20 @@ def supervise(serverID, reservation_data, parent_dict, jenkins_info, user_info =
 
     print supervisor_api.check_output_for_other_fails_or_errors_and_get_test_end_status()
 
-    if supervisor_api.are_any_failed_tests :
-        supervisor_api.remove_tag_from_robots_tests()
+    # if supervisor_api.are_any_failed_tests :
+        # supervisor_api.remove_tag_from_robots_tests()
 
     supervisor_api.send_information_about_executed_job()
 
     supervisor_api.set_parent_dictionary(busy_status=False)
     return 0
 
+if __name__ == '__main__':
+    serverID = 123
+    reservation_data = {'testline_type' : 'CLOUD_F',
+                        'duration' : 120}
+    parent_dict = {}
+    jenkins_info = {'parameters' : {'name' : 'LTEXYZ'}}
+    user_info = None
+    TLreservationID = 72118
+    supervise(serverID, reservation_data, parent_dict, jenkins_info, user_info, TLreservationID)
--- server_reservation_dispatcher/superVisor_api.py
+++ server_reservation_dispatcher/superVisor_api.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 :created on: '11/08/15'
@@ -19,7 +20,6 @@ import ute_mail.sender
 import ute_mail.mail
 import paramiko
 
-import tl_reservation as tlr
 from mailing_list import mail_dict
 from utilities.logger_messages import LOGGER_INFO
 from server_git_api import git_launch
@@ -33,158 +33,72 @@ config_logger(logger)
 ########################################################################################
 
 class Supervisor(object):
-    def __init__(self, serverID, reservation_data, parent_dict, jenkins_info, user_info=None, TLreservationID=None):
-        self.serverID = serverID
-        if not 'duration' in reservation_data:
-            self.failureStatus = 113
-            logger.error('{}'.format(LOGGER_INFO[self.failureStatus]))
-            self.finish_with_failure()
-        self.reservation_data = reservation_data
-        self.parent_dictionary = parent_dict    #dictionary used to communicate with parent process
-        self.jenkins_info = jenkins_info
-        self.TLreservationID = TLreservationID
-        self.user_info = user_info
-        self.TLname = None
-        self.TLaddress = None
+    def __init__(self,TLname, jenkins_job_info, user_info=None):
+        self.jenkins_info = jenkins_job_info    #dict:{job_name : "", parameters : {param1 : "", ...}}
+        self.user_info = user_info          #dict:{first_name : "", last_name : "", mail :""}
+        self.TLname = TLname
+        self.TLaddress = self.get_TLaddress()
         self.failureStatus = None
         self.are_any_failed_tests = False
         self.test_end_status = None
         self.job_tests_failed_list = None
         logger.debug("Created new Supervisor object with args: "
-                     "serverID = {}, reservation_data = {}, parent_dict = {}, jenkins_info = {}, user_info = {},"
-                     " TLreservationID = {}".format(self.serverID, self.reservation_data, self.parent_dictionary,
-                                                    self.jenkins_info, self.user_info, self.TLreservationID))
-
-
-    def get_serverID(self):
-        return self.serverID
-
-    def get_reservation_data(self):
-        return self.reservation_data
-
-    def get_parent_dictionary(self):
-        return self.parent_dictionary
-
-    def set_parent_dictionary(self, busy_status=True):
-        '''
-        :param busy_status: tells whether supervisor has finished work or not
-        :return self.parent_dictionary: full information about reservation that is managed by this supervisor
-        '''
-        jenkins_info = self.jenkins_info.copy()
-        if 'job_api' in jenkins_info:
-            del jenkins_info['job_api']
-        if 'console_output' in jenkins_info:
-            del jenkins_info['console_output']
-        self.parent_dictionary[self.serverID] = {
-            'reservationID' : self.TLreservationID,
-            'busy_status' : busy_status,
-            'tl_name' : self.TLname,
-            'duration' : self.reservation_data['duration'],
-            'test_status' : self.job_tests_failed_list,
-            'jenkins_info': jenkins_info,
-            'reservation_data' : self.reservation_data,
-            'user_info' : self.user_info
-        }
-        logger.debug("Parent dictionary = {}".format(self.parent_dictionary))
-        return self.parent_dictionary
+                     "jenkins_info = {}, user_info = {}, TLreservationID = {}".format(self.jenkins_info,
+                                                                                      self.user_info,
+                                                                                      self.TLreservationID))
 
     def get_jenkins_info(self):
         return self.jenkins_info
 
 
-    def get_TLreservationID(self):
-        return self.TLreservationID
-
     def get_user_info(self):
         return self.user_info
 
-    def set_user_info(self, first_name, last_name, e_mail):
-        self.user_info = {
-            'first_name' : first_name,
-            'last_name' : last_name,
-            'e-mail' :e_mail
-        }
-        logger.debug("Saving user informations : {} {}, email: {}".format(first_name,last_name,e_mail))
-
-        return self.user_info
 
     def get_TLname(self):
         return self.TLname
 
-    def set_TLname_from_TLreservationID(self):
-        reservation = tlr.TestLineReservation(self.TLreservationID)
-        self.TLname = reservation.get_reservation_details()['testline']['name']
-        return self.TLname
-
-    def set_TLname(self,name):
-        self.TLname = name
-        return self.TLname
-
-    def get_TLaddress_from_ID(self):
-        reservation = tlr.TestLineReservation(self.TLreservationID)
-        return reservation.get_address()
 
     def get_TLaddress(self):
-        return self.TLaddress
+        #TODO: open file with mapped TL->DNS_address and return address
+        pass
 
-    def set_TLaddress(self, address):
-        self.TLaddress = address
-        return self.TLaddress
 
-    def finish_with_failure(self, test_status = None):
-        self.parent_dictionary[self.serverID]['busy_status'] = False
-        if test_status:
-            self.test_end_status = test_status
+    def finish_with_failure(self):
+        #TODO clear job_info from file
         self.send_information_about_executed_job(test_status=self.test_end_status)
         sys.exit()
 
-    def create_reservation(self, testline_type=None, enb_build=None, ute_build=None,
-                                   sysimage_build=None, robotlte_revision=None,
-                                   state=None, duration=None):
 
-        reservation = tlr.TestLineReservation()
-
-        self.TLreservationID = reservation.create_reservation(testline_type=testline_type, enb_build=enb_build,
-                                                           ute_build=ute_build, sysimage_build=sysimage_build,
-                                                           robotlte_revision=robotlte_revision,
-                                                           state=state, duration=duration)
-        if self.TLreservationID == -103:
-            self.failureStatus = 101
-            logger.warning('{}'.format(LOGGER_INFO[self.failureStatus]))
-            self.finish_with_failure(test_status="PASS")
-        elif self.TLreservationID == -102:
-            self.failureStatus = 102
-            logger.warning('{}'.format(LOGGER_INFO[self.failureStatus]))
-            self.finish_with_failure(test_status="PASS")
-        else:
-            logger.info("Reservation created. ID: {}".format(self.TLreservationID))
-            return self.TLreservationID
-
-    def check_and_wait_for_TL_being_prepared_to_use(self):
-        reservation = tlr.TestLineReservation(self.TLreservationID)
-        _reservation_status_dict = {1: 'Pending for testline',
-                                   2: 'Testline assigned',
-                                   3: 'Confirmed',
-                                   4: 'Finished',
-                                   5: 'Canceled'}
-        while True:
-            if reservation.get_reservation_status() == _reservation_status_dict[1] or\
-                            reservation.get_reservation_status() == _reservation_status_dict[2]:
-                logger.debug("Waiting for TL (reservation ID: {}) to be ready for use. TL status: {}".format(self.TLreservationID, reservation.get_reservation_status()))
-                time.sleep(120)
-            elif reservation.get_reservation_status() == _reservation_status_dict[3]:
-                logger.info("{} ready".format(self.set_TLname_from_TLreservationID()))
-                return 0
-            elif reservation.get_reservation_status() == _reservation_status_dict[4] or\
-                            reservation.get_reservation_status() == _reservation_status_dict[5]:
-                self.failureStatus = 103
-                logger.warning('{} {}'.format(self.TLreservationID, LOGGER_INFO[self.failureStatus]))
-                self.finish_with_failure()
-
-
-    def set_job_TLname(self):
+    def set_jenkins_connection(self):
+        if not 'job_name' in self.jenkins_info:
+            self.jenkins_info['job_name'] = 'job_on_{tl_name}'.format(tl_name=self.TLname)
         try:
-            job = self.jenkins_info['job_api'].get_job(self.jenkins_info['job_name'])
+            jenkins_connection = jenkinsapi.api.Jenkins('http://plkraaa-jenkins.emea.nsn-net.net:8080', username='nogiec', password='!salezjanierlz3!')
+            self.jenkins_info['connection'] = jenkins_connection
+            logger.debug("jenkins connection has been set")
+            return self.jenkins_info['connection']
+        except:
+            self.failureStatus = 106
+            self.test_end_status = "JenkinsError"
+            logger.critical('{}'.format(LOGGER_INFO[self.failureStatus]))
+            self.finish_with_failure()
+
+
+    def set_jenkins_job(self):
+        try:
+            self.jenkins_info['job_handler'] = self.jenkins_info['connection'].get_job(self.jenkins_info['job_name'])
+            return self.jenkins_info['job_handler']
+        except:
+            self.failureStatus = 125
+            self.test_end_status = "JenkinsError"
+            logger.critical('{}'.format(LOGGER_INFO[self.failureStatus]))
+            self.finish_with_failure()
+
+
+    def set_node_for_job(self):
+        try:
+            job = self.jenkins_info['job_handler']
             job_config_xml = ET.fromstring(job.get_config())
             assignedNode_tag = job_config_xml.find('assignedNode')
             assignedNode_tag.text = str(self.TLname)
@@ -193,16 +107,18 @@ class Supervisor(object):
             return 0
         except:
             self.failureStatus = 104
+            self.test_end_status = "JenkinsError"
             logger.warning('{} : {}'.format(self.TLname, LOGGER_INFO[self.failureStatus]))
             self.finish_with_failure()
+
 
     def get_job_status(self):
         job_status = None
         try:
-            job = self.jenkins_info['job_api'].get_job(self.jenkins_info['job_name'])
-            job_status = job.get_last_build().get_status()
+            job_status = self.jenkins_info['job_handler'].get_last_build().get_status()
         except:
             self.failureStatus = 105
+            self.test_end_status = "JenkinsError"
             logger.error('{} : {}'.format(self.jenkins_info['job_name'],LOGGER_INFO[self.failureStatus]))
             self.finish_with_failure()
         finally:
@@ -211,60 +127,52 @@ class Supervisor(object):
                 self.are_any_failed_tests = False
                 self.check_output_for_other_fails_or_errors_and_get_test_end_status()
             else:
-                return job.get_last_build().get_status()
+                return job_status
 
-    def _is_queued_or_running(self):
+
+    def _is_queued_or_running(self, only_once=None):
+        if only_once:
+            try:
+                return self.jenkins_info['job_handler'].is_queued_or_running()
+            except:
+                self.failureStatus = 124
+                self.test_end_status = "JenkinsError"
+                logger.error('{} : {}'.format(self.jenkins_info['job_name'],LOGGER_INFO[self.failureStatus]))
+                self.finish_with_failure()
         try:
-            job = self.jenkins_info['job_api'].get_job(self.jenkins_info['job_name'])
-            print job.is_queued_or_running()
-            return job.is_queued_or_running()
+            while True:
+                if self.jenkins_info['job_handler'].is_queued_or_running():
+                    time.sleep(3)       #TODO LONGER SLEEP LATER
+                else:   break
         except:
-            self.failureStatus = 105
+            self.failureStatus = 124
+            self.test_end_status = "JenkinsError"
             logger.error('{} : {}'.format(self.jenkins_info['job_name'],LOGGER_INFO[self.failureStatus]))
             self.finish_with_failure()
 
-    def set_job_api(self):
-        if not 'job_name' in self.jenkins_info:
-            self.jenkins_info['job_name'] = 'test_on_{tl_name}'.format(tl_name=self.TLname)
-        try:
-            job_api = jenkinsapi.api.Jenkins('http://plkraaa-jenkins.emea.nsn-net.net:8080', username='nogiec', password='!salezjanierlz3!')
-            self.jenkins_info['job_api'] = job_api
-            logger.debug("job_api has been set")
-            return self.jenkins_info['job_api']
-        except:
-            self.failureStatus = 106
-            logger.critical('{}'.format(LOGGER_INFO[self.failureStatus]))
-            self.finish_with_failure()
 
-    def create_and_build_job(self):
+    def _build_job(self):
         try:
-            self.set_job_TLname()
-            self.set_job_api()._build_job(jobname=self.jenkins_info['job_name'],
-                                                   params=self.jenkins_info['parameters'])
+            self.jenkins_info['connection'].build_job(jobname=self.jenkins_info['job_name'],
+                                                      params=self.jenkins_info['parameters'])
             logger.info("Job {} was built".format(self.jenkins_info['job_name']))
-            print "cos2"
             return 0
         except:
-            print self.jenkins_info['job_api']
-            print self.jenkins_info['job_name']
-            print self.jenkins_info['parameters']
-            print "cos3"
             self.failureStatus = 107
+            self.test_end_status = "JenkinsError"
             logger.error('{}'.format(LOGGER_INFO[self.failureStatus]))
             self.finish_with_failure()
 
-    def get_jenkins_console_output(self):
+
+    def set_jenkins_console_output(self):
         try:
-            job = self.jenkins_info['job_api'].get_job(self.jenkins_info['job_name'])
-            while True:
-                if not self._is_queued_or_running(): break
-                else:
-                    time.sleep(2)   ####TODO longer time on real tests
-            self.jenkins_info['console_output'] = job.get_last_build().get_console()
+            self._is_queued_or_running()
+            self.jenkins_info['console_output'] = self.jenkins_info['job_handler'].get_last_build().get_console()
             logger.debug("Console output retrieved from {}".format(self.jenkins_info['job_name']))
             return self.jenkins_info['console_output']
         except:
             self.failureStatus = 108
+            self.test_end_status = "JenkinsError"
             logger.error('{}'.format(LOGGER_INFO[self.failureStatus]))
             self.finish_with_failure()
 
