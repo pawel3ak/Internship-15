315994baaeac315a74e918bcc55bad66087b0923
Pawel Nogiec <pawel.nogiec@nokia.com>
2015-09-04 11:12:12 +0200

    api almost finished

---
+++ server_reservation_dispatcher/superVisor.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+:created on: '11/08/15'
+
+:copyright: Nokia
+:author: Pawel Nogiec
+:contact: pawel.nogiec@nokia.com
+"""
+
+
+from superVisor_api import Supervisor
+
+
+#
+# __job_status = self.get_job_status()
+# if __job_status == "FAILURE":
+#     self.set_are_any_failed_tests(False)
+#     self.check_output_for_other_fails_or_errors_and_get_test_end_status()
+# elif __job_status == "UNKNOWN":
+#     self.set_failure_status(127)
+#     self.set_test_end_status("JenkinsError")
+#     self.finish_with_failure()
