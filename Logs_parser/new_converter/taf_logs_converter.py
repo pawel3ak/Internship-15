a641a7a808c3465cfdf5497269618f39efd64759
Klaudia Gadek <klaudia.gadek@nokia.com>
2015-09-07 12:14:27 +0200

    Final version of raw yaml taflogs  converter

--- Logs_parser/new_converter/taf_logs_converter.py
+++ Logs_parser/new_converter/taf_logs_converter.py
@@ -45,48 +45,55 @@ class LogsRecord(dict):
 class TafLogsConverter(object):
     def __init__(self, logs_location):
         self.logs_location = logs_location
-        self.time_file = ""
-        self.rawfile_whole_payload = ""
 
-    def create_yaml_file(self, raw_filename):
-        remaining_payload = self.rawfile_whole_payload
+    def create_yaml_file(self, filename):
+        raw_filename=filename+".raw"
+        raw_time_filename=raw_filename+".time"
+        filename_yaml = filename+".yml"
+        self.backup_conversion_target_files(filename_yaml)
+
+        with open(filename_yaml, 'a') as new_yaml_file, \
+             open(raw_time_filename) as raw_time_file, \
+             open(raw_filename) as raw_file:
+            self._create_yaml_file(new_yaml_file,raw_time_file,raw_file)
+
+
+    def _create_yaml_file(self, new_yaml_file,raw_time_file,raw_file):
         yaml.add_representer(LogsRecord, represent_omap)
         record_number = 1
-        filename_yaml = (raw_filename.replace(".raw", ""))+".yml"
-        self.backup_conversion_target_files(filename_yaml)
-        with open(filename_yaml, 'a') as new_yaml_file:
-            # format of a line: 13:25:27.85 | 39 |> 1438601127.853814 |
-            for line in self.time_file:
-                (timestamp, size, type_and_epoch, _) = tuple(line.split("|"))
-                log_data = LogsRecord({
-                    "Nr": record_number,
-                    "Time": timestamp,
-                    "Epoch": type_and_epoch[2:],
-                    "Payload": non_printable_to_ascii(remaining_payload[0:int(size)]),
-                    "Type_of_record": type_and_epoch[0]})
-                remaining_payload = remaining_payload[int(size):]
-                new_yaml_file.write(yaml.dump(log_data, default_flow_style=False, explicit_start=True))
-                record_number += 1
+        # format of a line: 13:25:27.85 | 39 |> 1438601127.853814 |
+        for line in raw_time_file:
+            (timestamp, size, type_and_epoch, _) = tuple(line.split("|"))
+            payload=raw_file.read(int(size))
+            log_data = LogsRecord({
+                "Nr": record_number,
+                "Time": timestamp,
+                "Epoch": type_and_epoch[2:],
+                "Payload": non_printable_to_ascii(payload),
+                "Type_of_record": type_and_epoch[0]})
+            new_yaml_file.write(yaml.dump(log_data, default_flow_style=False, explicit_start=True))
+            record_number += 1
 
-    def get_and_convert_raw_file(self, raw_filename):
-        self.time_file = open(raw_filename+".time", "r")
-        log_file = open(raw_filename, "r")
-        self.rawfile_whole_payload = log_file.read()
-
-    def create_raw_file(self, yaml_filename):
-        filename_raw = yaml_filename.replace(".yml", ".raw")
+    def create_raw_file(self, filename):
+        filename_raw = filename+".raw"
         filename_raw_time = filename_raw+".time"
-        with open(yaml_filename, 'r') as filename_yaml:
-            yaml_data = yaml.load_all(filename_yaml)
-            self.backup_conversion_target_files(filename_raw)
-            self.backup_conversion_target_files(filename_raw_time)
-            with open(filename_raw, 'w') as raw_file, open(filename_raw_time, 'w') as raw_time_file:
-                for record in yaml_data:
-                    record_payload = restore_non_printable(record['Payload'])
-                    record_size = len(record_payload)
-                    rawtime_record_line = "%s|%7.7s |%s %s|\n" % (record['Time'], record_size, record['Type_of_record'], record['Epoch'])
-                    raw_file.write(record_payload)
-                    raw_time_file.write(rawtime_record_line)
+        yaml_filename=filename+".yml"
+        self.backup_conversion_target_files(filename_raw)
+        self.backup_conversion_target_files(filename_raw_time)
+
+        with open(yaml_filename, 'r') as filename_yaml,\
+             open(filename_raw, 'w') as raw_file, \
+             open(filename_raw_time, 'w') as raw_time_file:
+            self._create_raw_file(filename_yaml,raw_file,raw_time_file)
+
+    def _create_raw_file(self, filename_yaml,raw_file,raw_time_file):
+        yaml_data = yaml.load_all(filename_yaml)
+        for record in yaml_data:
+            record_payload = restore_non_printable(record['Payload'])
+            record_size = len(record_payload)
+            rawtime_record_line = "%s|%7.7s |%s %s|\n" % (record['Time'], record_size, record['Type_of_record'], record['Epoch'])
+            raw_file.write(record_payload)
+            raw_time_file.write(rawtime_record_line)
 
     def backup_conversion_target_files(self, filename_to_check):
         for filename in os.listdir(self.logs_location):
@@ -98,14 +105,15 @@ class TafLogsConverter(object):
         for filename in os.listdir(self.logs_location):
             if filename.endswith(".raw"):
                 raw_filename = os.path.join(self.logs_location, filename)
-                self.get_and_convert_raw_file(raw_filename)
-                self.create_yaml_file(raw_filename)
+                filename=raw_filename.replace(".raw","")
+                self.create_yaml_file(filename)
 
     def convert_to_raw_format(self):
         for filename in os.listdir(self.logs_location):
             if filename.endswith(".yml"):
                 yaml_filename = os.path.join(self.logs_location, filename)
-                self.create_raw_file(yaml_filename)
+                filename=yaml_filename.replace(".yml","")
+                self.create_raw_file(filename)
 
 
 if __name__ == "__main__":
