ff2f1046068747a7dd5b95b1b15c6d12d33b5a75
Pawel Nogiec <pawel.nogiec@nokia.com>
2015-09-10 14:16:51 +0200

    (probably) working git_api v2

--- server_reservation_dispatcher/ours_git_api.py
+++ server_reservation_dispatcher/ours_git_api.py
@@ -20,44 +20,49 @@ def git_action(ssh_process, command, comment = None):
     if comment:
         command = '{} -m "{}"'.format(command,comment)
 
+
     exepcted_matches_list = ['Writing objects:.*done',#0
                              'Unpacking objects:.*done',#1
-                             'no changes added to commit',#2
-                             'Aborting commit',#3
-                             'fatal',#4
-                             '.*up-to-date',#5
-                             'Password.*:',#6
-                             '.*~//PycharmProjects/projekty/Internship-15.*',#7
-                             '.*{}.*'.format(comment),#8
-                             pexpect.EOF,#8
-                             pexpect.TIMEOUT]#9
+                             '.*up-to-date',#2
+                             'Password.*:',#3
+                             '.*~//PycharmProjects/projekty/Internship-15.*',#4
+                             '.*{}.*'.format(comment),#5
+                             '.*no changes added to commit.*',#6
+                             'Aborting commit',#7
+                             'fatal',#8
+                             pexpect.EOF,#9
+                             pexpect.TIMEOUT]#10
     ssh_process.sendline(command)
+
     while True:
-        match = ssh_process.expect(exepcted_matches_list, timeout=1)
+        match = ssh_process.expect(exepcted_matches_list, timeout=3)
+        logger.info('{}'.format(exepcted_matches_list.index(match)))
         if match == 0:
-            return True
+            continue
         elif match == 1:
             return True
         elif match == 2:
-            logger.error("Commit failure - no changes")
-            return False
+            return True
         elif match == 3:
-            logger.error("Commit failure - aborting")
-            return False
+            passwd = "Flexi1234"
+            ssh_process.sendline(passwd)
         elif match == 4:
-            logger.error("Fatal failure - no file to add")
-            return False
+            return True
         elif match == 5:
             return True
         elif match == 6:
-            passwd = "Flexi1234"
-            ssh_process.sendline(passwd)
+            logger.error("Commit failure - no changes")
+            return False
         elif match == 7:
-            return True
+            logger.error("Commit failure - aborting")
+            return False
         elif match == 8:
-            return True
+            logger.error("Fatal failure - no file to add")
+            return False
         elif match == 9:
             pass
+        elif match == 10:
+            return True
 
 
 def git_launch(TL_address, path, pull_only=True):
@@ -68,13 +73,11 @@ def git_launch(TL_address, path, pull_only=True):
         return False
     if pull_only:
         return True
+
     #git add
     ssh_process.sendline('git add {}'.format(path))
     #git commit
-    result = git_action(ssh_process, 'git commit', 'Testing git_api')
-    if not result:
-        return False
-    # git push
+    git_action(ssh_process, 'git commit', 'Testing git_api8')
     result = git_action(ssh_process, 'git push')
     if not result:
         return False
