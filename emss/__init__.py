9868c63552afa3d1aebd90a90430003ac477e21a
tarsa Pawel Tarsa <tarsa@nokia.com>
2016-01-15 15:07:26 +0100

    Cleanup

--- emss/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-:author: Pawel Tarsa
-:contact: pawel.tarsa@nsn.com
-"""
--- server_Internship-14/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-module docstring...
-"""
-from utils.jen import jen_exe, jen_job, jen_checker
-from utils.svn import svn_log, svn_checker
-from utils.xlm import DictToXml, xml2dict
